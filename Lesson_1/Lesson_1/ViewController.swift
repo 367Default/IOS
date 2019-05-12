231e545bbadc16fe42bfffb5511f908bf5d9e96b
Jusodal <40366577+367Default@users.noreply.github.com>
2019-05-12 18:28:26 +0300

    Lesson_1

---
+++ Lesson_1/Lesson_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_1
+//
+//  Created by Ruslan Tagiev on 12/05/2019.
+//  Copyright © 2019 Ruslan Tagiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
