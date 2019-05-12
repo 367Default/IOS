a931bb961f211f9b95df0b49837e515a6a50b4e3
Jusodal <40366577+367Default@users.noreply.github.com>
2019-05-12 21:41:27 +0300

    Lesson_2

--- Lesson_1/Lesson_1/ViewController.swift
+++ Lesson_1/Lesson_1/ViewController.swift
@@ -13,23 +13,24 @@ class Session {
     static let instance = Session()
     
     private init(){}
-    
-    var fio = ""
-    var id = 0
-    var money = 0
-    var token = 0
+
+    var vkToken = ""
 }
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vkButton: UIButton!{
+        didSet {
+            vkButton.layer.borderWidth = 2
+            vkButton.layer.borderColor = UIColor.white.cgColor
+            vkButton.layer.cornerRadius = 10
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let session = Session.instance
-        session.fio = "Иванов Иван Иванович"
-        session.id = 189483
-        session.money = 9999
-        session.token = 837489879384
+
         
     }
+    
 }
